8244e786ef71cec7a02b9acfcee3e571868b0067
오승철 <tmdcjf103@gmail.com>
2021-03-10 19:44:52 +0900

    Add 01_django_model

---
+++ Django/01_django_model/practice/articles/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+# Model Schema 정의
+class Article(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
